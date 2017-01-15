ed34eaedc6c15b339386e0e422571019c0d57991
mustangws <mustangws@net.hr>
2017-01-15 21:26:40 +0100

    Update Senzori.java

--- app/src/main/java/smarthome/com/smarthome/Main/Senzori.java
+++ app/src/main/java/smarthome/com/smarthome/Main/Senzori.java
@@ -2,9 +2,34 @@ package smarthome.com.smarthome.Main;
 
 public class Senzori {
 
-    private Measurement[] measurement;
+@Expose
+private Integer id;
+@Expose
+private String type;
+@Expose
+private List<Measurement> measurements = null;
 
-    public Measurement[] getMeasurement() {
-        return measurement;
-    }
+public Integer getId() {
+    return id;
+}
+
+public void setId(Integer id) {
+    this.id = id;
+}
+
+public String getType() {
+    return type;
+}
+
+public void setType(String type) {
+    this.type = type;
+}
+
+public List<Measurement> getMeasurements() {
+    return measurements;
+}
+
+public void setMeasurements(List<Measurement> measurements) {
+    this.measurements = measurements;
+}
 }
