ef470073fd4afea6bd8bffa2979c7659025cec6b
mustangws <mustangws@net.hr>
2017-01-15 21:25:56 +0100

    Update Measurement.java

--- app/src/main/java/smarthome/com/smarthome/Main/Measurement.java
+++ app/src/main/java/smarthome/com/smarthome/Main/Measurement.java
@@ -1,15 +1,26 @@
 package smarthome.com.smarthome.Main;
 
 public class Measurement {
+
+    @Expose
+    private String value;
+    @Expose
     private String time;
-    private Double value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 
     public String getTime() {
         return time;
     }
 
-    public Double getValue() {
-        return value;
+    public void setTime(String time) {
+        this.time = time;
     }
 
 }
